a32d6ed400e4409d1e87174a532dd0c3ad1d3fd9
Lee-Martin Clarke <mobeel5356@gmail.com>
2017-03-06 18:44:07 -0500

    updated notes #1

--- info.sh
+++ info.sh
@@ -22,39 +22,45 @@ echo " "
 #make variable enviromental
 export x
 
-#------------------------------------------------------------------------|
-
+#Space-------------------------------------------------------------------|
+echo " "
 
 #Bourne shell uses 'expr' for Arithmatic
 #so just use 'expr' when doing add,sub,mul,div,etc.
-#matching or equal
+x=1
+expr $x + 1
+expr $x \> 1
+#1=true; 0=false
+
+#matching or equal(returns amount matched)<----------------------
 expr "swimming" : 'sw.*ing'
+#or
+expr match "swimming" 'sw.*ing'
 
 #Space-------------------------------------------------------------------|
 echo " "
 
-#for loop
+#for-loop<----------------------------------------------------------
 for i in {5..1..1}
 do
   echo Countdown: $i
 done
 
-#Space-------------------------------------------------------------------|
 echo " "
 
-#for loop (familiar)
+#for-loop (familiar)<----------------------
 for ((j=1; j<=5; j++))
 do
   echo Upcounter: $j
 done
 
-#Space-------------------------------------------------------------------|
+
 echo " "
 
 #if false then ...... fi' statement skips the code "...."
 if false
 then
-#infinite loop
+#infinite loop<----------------------
 for ((; ;))
 do
   echo This is an infinite loop [CTRL+C to stop]
@@ -62,13 +68,81 @@ done
 fi
 echo Infinite loop did not run
 
+echo
+
+#special if case
+if false
+then
+test=$@
+for values in $test
+do
+  echo test: $valeus
+done
+fi
+
+
+
 #Space-------------------------------------------------------------------|
 echo " "
 
-#while loop(binary operators -lt -le -gt -ge)
+#while loop(binary operators -lt -le -gt -ge -eq)<----------------------
 a=0
 while [ $a -le 10 ]
 do
   echo $a
   ((a++))
 done
+
+#Space-------------------------------------------------------------------|
+echo " "
+
+#bash command for arithmatic (())
+x=2
+((x=x+1))
+echo $x
+
+#Space-------------------------------------------------------------------|
+echo " "
+
+#Check if file exists (0=true; 1=false)
+test -e ~/"UNIX Info"/"info.sh"
+echo $?
+
+echo
+
+#Check (binary operators -lt -le -gt -ge -eq)<--------------
+test 1 -eq 1
+echo $?
+
+#Space-------------------------------------------------------------------|
+echo " "
+
+#if else statement (checks if file exists if working directory))<--------------
+echo "enter a word:"
+read v1
+if [ -e $vi ]
+then
+  echo "A file by that name exists"
+else
+  echo "No file by that name exists"
+fi
+
+#Space-------------------------------------------------------------------|
+echo " "
+
+#case/switch<--------------
+echo "type the word for 1 or 2"
+read v1
+case $v1 in
+  [Oo]ne)
+    echo "you entered 1"
+    ;;
+  [Tt]wo)
+    echo "you entered 2"
+    ;;
+  *)
+    echo "You either misspelled the word or entered a different number"
+esac
+
+#Space-------------------------------------------------------------------|
+echo " "
